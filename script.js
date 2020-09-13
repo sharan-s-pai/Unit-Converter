47723a6270be9979abdcf9584030f7b3f53214dd
SharanPai <sharanpai2001@gmail.com>
2020-09-13 13:28:53 +0530

    added remove button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let plusButton = document.getElementById("addDropdowns");
 
+let minusButton = document.getElementById("removeDropdowns");
+
 let input = document.getElementById("input");
 
 let options = ["centimeter", "meter", "kilometer", "feet", "inches"];
@@ -9,21 +11,33 @@ let options = ["centimeter", "meter", "kilometer", "feet", "inches"];
 // });
 
 let sideBar = document.getElementById("innerNav");
-let i = 1;
+let i = 2;
 let addSelect = () => {
   if (i <= options.length) {
-    let select = `<h6 class="text-center mb-3" style="color:#fff">TO</h6>
+    let section = `<section id="output-${i}" class="text-center">
+    <h6 class="text-center mb-3" style="color:#fff">TO</h6>
     <select name="unit" id="output${i}" class="form-control mb-3">
         <option value="cm">centimeter</option>
         <option value="m">meter</option>
         <option value="km">kilometer</option>
         <option value="ft">feet</option>
         <option value="inch">inch</option>
-    </select>`;    
-    sideBar.innerHTML += select;
-
+    </select></section>`;    
+    sideBar.innerHTML += section;
+    console.log(i);
   }else{
       return 0;
   }
   i++;
 };
+
+let removeSelect = ()=>{
+    if(i<3){
+        return 0;
+    }
+    i--;
+    console.log(i);
+    let select = document.getElementById(`output-${i}`)
+    select.remove();
+    
+}
